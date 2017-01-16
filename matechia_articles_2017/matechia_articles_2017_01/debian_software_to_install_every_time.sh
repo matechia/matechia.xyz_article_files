f08d130406702e584fecbbb684eca5a6a04a4047
matechia <matechia@yandex.com>
2017-01-16 17:24:56 -0600

    Create debian_software_to_install_every_time.sh

---
+++ matechia_articles_2017/matechia_articles_2017_01/debian_software_to_install_every_time.sh
@@ -0,0 +1,50 @@
+#!/bin/bash/
+sudo apt-get install alarm-clock-applet \
+apache2 \
+arandr \
+build-essential \
+bum \
+chromium \
+clamav \
+cups \
+dillo  \
+elinks \
+espeak \
+filezilla \
+freeplane \
+gimp \
+git \
+gnome-screenshot \
+gparted \
+gtick \
+gtimelog \
+hunspell hunspell-en-us myspell-en-gb \
+ibus ibus-pinyin \
+icedove \
+ipython3 \
+ipython3-qtconsole \
+keepassx \
+librecad \
+links2 \
+lynx \
+mariadb-server \
+mnemosyne \
+openjdk-7-jdk \
+openvpn \
+pavucontrol \
+phatch \
+pinta \
+qemu qemu-kvm \
+simple-image-reducer \
+soundconverter \
+spyder3 \
+sshfs \
+stopwatch \
+ufw \
+vim \
+vinagre \
+vlc \
+whois \
+wine \
+xfce4-power-manager \
+youtube-dl \
