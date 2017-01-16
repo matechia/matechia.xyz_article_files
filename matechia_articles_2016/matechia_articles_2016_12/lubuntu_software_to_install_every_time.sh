b0bff49ea94e6a65d941f5a3fdb0c786a6fba421
matechia <matechia@yandex.com>
2017-01-16 17:41:37 -0600

    Create lubuntu_software_to_install_every_time.sh

---
+++ matechia_articles_2016/matechia_articles_2016_12/lubuntu_software_to_install_every_time.sh
@@ -0,0 +1,54 @@
+#!/bin/bash/
+#This file was created by matechia.xyz
+#It is released under the GPLv3 license
+#You are free to copy it and/or share it
+#Just please give credit to matechia.xyz
+sudo apt-get install apache2 \
+arandr \
+build-essential \
+bum \
+clamav \
+chromium-browser \
+cups \
+dillo \
+elinks \
+espeak \
+filezilla \
+freeplane \
+gimp \
+git \
+gnome-screenshot \
+gparted \
+gtick \
+gtimelog \
+hunspell hunspell-en-us myspell-en-gb \
+ibus ibus-pinyin \
+ipython3 \
+ipython3-qtconsole \
+keepassx \
+librecad \
+libreoffice \
+links2 \
+lynx \
+mariadb-server \
+midori \
+mnemosyne \
+openvpn \
+pavucontrol \
+phatch \
+pinta \
+qemu qemu-kvm \
+scrot \
+shotwell \
+simple-image-reducer \
+soundconverter \
+sshfs \
+stopwatch \
+thunderbird \
+xfce4 xfce4-power-manager \
+vinagre \
+vim \
+vlc \
+whois \
+wine \
+youtube-dl \
