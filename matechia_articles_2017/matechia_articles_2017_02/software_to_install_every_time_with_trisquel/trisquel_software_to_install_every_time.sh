938746784370672cfaed57e0d86bdd18947a08ef
matechia.xyz <matechia@yandex.com>
2017-02-04 17:47:13 -0600

    Create trisquel_software_to_install_every_time.sh

---
+++ matechia_articles_2017/matechia_articles_2017_02/software_to_install_every_time_with_trisquel/trisquel_software_to_install_every_time.sh
@@ -0,0 +1,65 @@
+#!/bin/bash/
+#This file was created by matechia.xyz
+#It is released under the GPLv3 license
+#You are free to copy it, modify it, and/or share it
+#Just please give credit to matechia.xyz
+#this is a list of software to install every time in Trisquel 7.0
+#This comes from the article on matechia.xyz at https://matechia.xyz/software-install-every-time-trisquel/
+
+sudo apt-get install abrowser \
+alarm-clock-applet \
+apache2 \
+arandr \
+build-essential \
+bum \
+clamav \
+dillo \
+elinks \
+espeak \
+espeak-gui \
+fbreader \
+fdupes \
+filezilla \
+filezilla \
+freeplane \
+fslint \
+f-spot \
+gimp \
+git \
+gnome-screenshot \
+gparted \
+gtick \
+gtimelog \
+hunspell hunspell-en-us myspell-en-gb \
+ibus ibus-pinyin \
+icedove \
+ipython3 \
+keepassx \
+librecad \
+libreoffice \
+links2 \
+lynx \
+mariadb-server \
+mnemosyne \
+openvpn \
+pavucontrol \
+phatch \
+pinta \
+qemu \
+qemu-kvm \
+scrot \
+shotwell \
+simple-image-reducer \
+soundconverter \
+sshfs \
+stopwatch \
+sysinfo \
+ufw \
+vim \
+vinagre \
+vlc \
+whois \
+wine \
+xfce4 xfce4-power-manager \
+xscreensaver xscreensaver-data-extra xscreensaver-gl-extra \
+youtube-dl
